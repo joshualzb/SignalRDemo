0b83b1992b104d8b2d2854f226244524793a2dc6
Joshua <joshualzb@163.com>
2016-11-03 09:37:09 +0800

    添加项目文件。

---
+++ SignalRDemo/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class MyHub : Hub
+    {
+        public void Sync(string msg)
+        {
+            Clients.All.Sync(msg);
+        }
+    }
+}
